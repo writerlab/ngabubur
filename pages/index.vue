7308f9ab105e1553bb72b7834ad4db889c3a9fb7
hilmizul <netspytux@gmail.com>
2022-04-15 16:01:15 +0700

    halaman depan fetched

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,44 @@
 <template>
   <main>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-          <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
 
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item href="#">Link</b-nav-item>
-              <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-              <b-nav-form>
-                <b-form-input
-                  size="sm"
-                  class="mr-sm-2"
-                  placeholder="Search"
-                ></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                  >Search</b-button
-                >
-              </b-nav-form>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
-    <div class="container mb-5">
+    <div class="container">
       <h1 class="mb-5 mt-5">ngabubur</h1>
-      <div class="row">
-        <div class="col-md-4 mt-5">
-          <div class="card">
+      <div class="row mt-5 mb-5">
+        <div class="col-md-4" v-for="item in items" :key="item.id">
+          <div class="card mb-4">
             <div class="card-body">
-              <img src="" width="100%" height="400px" alt="" srcset="" />
+              <img :src="item.foto" width="100%" height="400px" alt="" srcset="" />
             </div>
             <div class="card-header">
-              <h1>judul</h1>
+              <strong>{{ item.nama_produk }}</strong>
+              <h5>Rp{{ item.harga_produk }}</h5>
             </div>
             <div class="card-footer">
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit asperiores at a aliquid nulla corrupti amet perferendis tempore ut aspernatur ratione, voluptate dolores eligendi sunt qui, sed ipsa! Veniam recusandae ex doloremque inventore, ea dolorum maxime nobis amet eos quidem.
-              </p>
-              <nuxtlink class="btn btn-dark">
-                beli
-              </nuxtlink>
+              <NuxtLink :to="item.link_eksternal" class="btn btn-dark"> beli </NuxtLink>
             </div>
           </div>
         </div>
@@ -55,5 +50,28 @@
 <script>
 export default {
   name: "IndexPage",
+  data() {
+    return {
+      items: [],
+    };
+  },
+
+  mounted() {
+    this.getProducts()
+  },
+
+  methods: {
+    async getProducts() {
+      let { data, error } = await this.$supabase
+        .from("Produk")
+        .select("*");
+
+      if (data) {
+        this.items = data
+        console.log(data)
+      }
+      if (error) pass;
+    },
+  },
 };
 </script>
