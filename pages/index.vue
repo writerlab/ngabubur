b6e45bfe84389d72b2bfd16af2035eb9b34e4bf1
hilmizul <netspytux@gmail.com>
2022-04-16 12:30:18 +0700

    add category and move component

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,22 @@
 <template>
   <main>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input
-              size="sm"
-              class="mr-sm-2"
-              placeholder="Search"
-            ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
-              >Search</b-button
-            >
-          </b-nav-form>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-
+    <Navbar />
 
     <div class="container">
       <h1 class="mb-5 mt-5">ngabubur</h1>
       <div class="row mt-5 mb-5">
         <div class="col-md-4" v-for="item in items" :key="item.id">
           <div class="card mb-4">
-            <div class="card-body">
+            <div class="card-header p-0">
               <img :src="item.foto" width="100%" height="400px" alt="" srcset="" />
             </div>
-            <div class="card-header">
-              <strong>{{ item.nama_produk }}</strong>
+            <div class="card-body">
+              <div class="badge badge-danger">{{ item.kategori.kategori_produk }}</div>
+              <div class="heading-5">{{ item.nama_produk }}</div>
               <h5>Rp{{ item.harga_produk }}</h5>
             </div>
             <div class="card-footer">
-              <NuxtLink :to="item.link_eksternal" class="btn btn-dark"> beli </NuxtLink>
+              <NuxtLink :to="item.link_eksternal" class="btn btn-danger btn-block"> beli </NuxtLink>
             </div>
           </div>
         </div>
@@ -48,8 +26,12 @@
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
   name: "IndexPage",
+  components: {
+    Navbar,
+  },
   data() {
     return {
       items: [],
@@ -64,11 +46,13 @@ export default {
     async getProducts() {
       let { data, error } = await this.$supabase
         .from("Produk")
-        .select("*");
+        .select(`
+          id, nama_produk, harga_produk, foto,
+          link_eksternal, kategori(kategori_produk)
+        `)
 
       if (data) {
         this.items = data
-        console.log(data)
       }
       if (error) pass;
     },
